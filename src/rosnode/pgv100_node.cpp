da8fd7869c3dd69dea4760f7b00787d6349b2bbb
Ermanas <ermanasyabanci@gmail.com>
2020-01-03 16:49:56 +0300

    - add calibration reset - make scan variables global to use on different functions - add some comments

--- src/rosnode/pgv100_node.cpp
+++ src/rosnode/pgv100_node.cpp
@@ -18,7 +18,7 @@
 //
 
 #include "ros/ros.h"
-#include "std_msgs/String.h"
+#include <std_msgs/String.h>
 #include <pf_pgv100/pgv_scan_data.h> // for message file
 #include <pf_pgv100/pgv_dir_msg.h>
 // C library headers
@@ -39,40 +39,53 @@
 
 using namespace std;
 
-// Set the port which your device is connected.
-
-//using namespace std;
-// To handle CTRL + C Interrupt
-
+//////////////////////////////////////////////////
 // Function Declerations
+//////////////////////////////////////////////////
 void my_handler(int s);
 unsigned long int string2decimal(string input);
 void direction_callback(const pf_pgv100::pgv_dir_msg::ConstPtr& msg);
+void calibration_callback(const std_msgs::String::ConstPtr &calmsg);
+
+//////////////////////////////////////////////////
 // Global Variables
+//////////////////////////////////////////////////
 int serial_port = open("/dev/ttyUSB0", O_RDWR);
 pf_pgv100::pgv_dir_msg sub_direction;
-string selected_dir;
-double agv_x_pos_des = 0.0;
+std_msgs::String calib_msg;
 unsigned char dir_straight[ 2 ] = {0xEC, 0x13}; // Straight ahead 
 unsigned char dir_left[ 2 ] = { 0xE8, 0x17}; // Follow Left
 unsigned char dir_right[ 2 ] = { 0xE4, 0x1B}; // Follow Right
 unsigned char dir_nolane[ 2 ] = { 0xE0, 0x1F}; // No lane is selected
 unsigned char pos_req[ 2 ] = { 0xC8, 0x37}; // Position Request
+double agv_ang_des = 0.0; // degree
+double agv_x_pos_des = 0.0; // mm
+double agv_y_pos_des; // mm
+string selected_dir;
+int agv_c_lane_count_des = 0;
+int agv_no_color_lane_des = 1;
+int agv_no_pos_des = 1;
+int tag_detected_des = 0;
+double cal_err_x = 0;
+double cal_err_y = 0;
+double cal_err_ang = 0;
 
 int main(int argc, char **argv)
 {
-  
-   // To handle CTRL + C Interrupt
+//////////////////////////////////////////////////  
+// To handle CTRL + C Interrupt
+//////////////////////////////////////////////////
     struct sigaction sigIntHandler;
 
     sigIntHandler.sa_handler = my_handler;
     sigemptyset(&sigIntHandler.sa_mask);
     sigIntHandler.sa_flags = 0;
-      
+//////////////////////////////////////////////////
 
 // Check for errors
 if (serial_port < 0) {
-    printf("Error %i from open: %s\n", errno, strerror(errno));
+    printf("Error %i from open: %s\n - Check your device is connected or not.\n", errno, strerror(errno));
+    return 1;
 }
 
 // Create new termios struc, we call it 'tty' for convention
@@ -83,8 +96,9 @@ memset(&tty, 0, sizeof tty);
 if(tcgetattr(serial_port, &tty) != 0) {
     printf("Error %i from tcgetattr: %s\n", errno, strerror(errno));
 }
-
+//////////////////////////////////////////////////
 // Serial Communication Configuration
+//////////////////////////////////////////////////
 tty.c_cflag |= PARENB;  // Set parity bit, enabling parity
 tty.c_cflag &= ~CSTOPB; // Clear stop field, only one stop bit used in communication (most common)
 tty.c_cflag |= CS7; // 8 bits per byte (most common)
@@ -111,15 +125,20 @@ cfsetospeed(&tty, B115200);
 if (tcsetattr(serial_port, TCSANOW, &tty) != 0) {
     printf("Error %i from tcsetattr: %s\n", errno, strerror(errno));
 }   
+//////////////////////////////////////////////////
 
 selected_dir = "No lane is selected";
 write(serial_port,  dir_nolane , sizeof( dir_straight));  
-
-ROS_INFO(" Direction set to <> Straight Ahead <>");
+cout << endl;
+ROS_INFO(" Direction set to <> No Lane Selected <>");
   ros::init(argc, argv, "pgv100_node");
+//////////////////////////////////////////////////
+// ROS Node Declerations
+//////////////////////////////////////////////////
   ros::NodeHandle pub, sub;
   ros::Publisher pgv100_pub = pub.advertise<pf_pgv100::pgv_scan_data>("pgv100_scan", 100); // second parameter is buffer
   ros::Subscriber pgv_dir = sub.subscribe<pf_pgv100::pgv_dir_msg>("pgv_dir",100,direction_callback);
+  ros::Subscriber calibrate_sub = sub.subscribe<std_msgs::String>("calibrate",100, calibration_callback);
   ros::Rate loop_rate(10);
 
   /**
@@ -147,13 +166,15 @@ ROS_INFO(" Direction set to <> Straight Ahead <>");
     int agv_c_lane_count_des = string2decimal(agv_c_lane_count_str);
     int agv_no_color_lane_des = string2decimal(agv_c_lane_detect_str);
     int agv_no_pos_des = string2decimal(agv_no_pos_str);
-    int tag_detected_des = string2decimal(tag_detected); 
+    tag_detected_des = string2decimal(tag_detected); 
 
     // Get the angle from the byte array [Byte 11-12]
     bitset<7> ang_1(read_buf[10]);
     bitset<7> ang_0(read_buf[11]);
     string agv_ang_str = ang_1.to_string() + ang_0.to_string();
-    double agv_ang_des = string2decimal(agv_ang_str)/(10.0);
+    agv_ang_des = string2decimal(agv_ang_str) / (10.0);
+    if (agv_ang_des > 180.0) // this makes x-pos zero centered
+      agv_ang_des -= 360.0;
 
     // Get the X-Position from the byte array [Bytes 3-4-5-6]
     bitset<3> x_pos_3(read_buf[2]);
@@ -166,11 +187,12 @@ ROS_INFO(" Direction set to <> Straight Ahead <>");
       if (agv_x_pos_des > 2000.0) // this makes x-pos zero centered
         agv_x_pos_des = agv_x_pos_des - pow(2,24) - 1;
     }
+    
     // Get Y-Position from the byte array [Bytes 7-8]
     bitset<7> y_pos_1(read_buf[6]);
     bitset<7> y_pos_0(read_buf[7]);
     string agv_y_pos_str = y_pos_1.to_string() + y_pos_0.to_string();
-    double agv_y_pos_des = string2decimal(agv_y_pos_str);
+    agv_y_pos_des = string2decimal(agv_y_pos_str);
     if (agv_y_pos_des > 2000.0) // this makes y-pos zero centered
       agv_y_pos_des = agv_y_pos_des - 16383.0;
     // We get opposite values when we try the read the y-pos value from the colored and code strip.
@@ -180,9 +202,9 @@ ROS_INFO(" Direction set to <> Straight Ahead <>");
 
     pf_pgv100::pgv_scan_data mesaj;
 
-    mesaj.angle = agv_ang_des; // degree
-    mesaj.x_pos = agv_x_pos_des/10.0; // mm
-    mesaj.y_pos = agv_y_pos_des/10.0; // mm
+    mesaj.angle = agv_ang_des - cal_err_ang;          // degree
+    mesaj.x_pos = (agv_x_pos_des - cal_err_x) / 10.0; // mm
+    mesaj.y_pos = (agv_y_pos_des - cal_err_y) / 10.0; // mm
     mesaj.direction = selected_dir;
     mesaj.color_lane_count = agv_c_lane_count_des;
     mesaj.no_color_lane = agv_no_color_lane_des;
@@ -235,5 +257,16 @@ void direction_callback(const pf_pgv100::pgv_dir_msg::ConstPtr &msg)
   {
     selected_dir = "Straigh Ahead";
     write(serial_port, dir_straight, sizeof(dir_straight));
+  }
+}
+
+void calibration_callback(const std_msgs::String::ConstPtr &calmsg)
+{
+  calib_msg.data == "reset";
+  if(tag_detected_des != 0)
+  {
+    cal_err_x = agv_x_pos_des;
+    cal_err_y = agv_y_pos_des;
+    cal_err_ang = agv_ang_des;
   }
 }
